390b92cd9f5bccbb79e83f07185c88790b8e993f
Sarayugandla <sarayugandla6@gmail.com>
2024-11-22 19:56:19 +0530

    First commit

---
+++ src/main/java/com/maven/demo/SampleMavenJAvaProject/App.java
@@ -0,0 +1,13 @@
+package com.maven.demo.SampleMavenJAvaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
